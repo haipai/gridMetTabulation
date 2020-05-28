6e6bbb1878f48917acf2249ff41478cf7106b120
haipai <jyjeepc@gmail.com>
2020-05-28 11:40:36 -0500

    Create sampleCounty.r
    R scripts to tabulate weather variables with county maps.

---
+++ sampleCounty.r
@@ -0,0 +1,9 @@
+rm(list=ls())
+library(ncdf4) # package for netcdf manipulation
+library(raster) # package for raster manipulation
+library(rgdal) # package for geospatial analysis
+library(ggplot2) # package for plotting
+library(data.table) 
+
+setwd('your directory with downloaded nc files') 
+
